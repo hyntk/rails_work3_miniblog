2785854a1a82d2f2f63c7d916529de5df0fc870d
hyntk <hoge@hoge.com>
2019-05-28 15:42:50 +0900

    投稿確認画面を編集時のみ表示するよう機能修正

--- app/helpers/miniblogs_helper.rb
+++ app/helpers/miniblogs_helper.rb
@@ -1,2 +1,9 @@
 module MiniblogsHelper
+        def choose_new_or_edit
+          if action_name == 'new' || action_name == 'confirm'
+            confirm_miniblogs_path
+          elsif action_name == 'edit'
+            miniblog_path
+          end
+        end
 end
