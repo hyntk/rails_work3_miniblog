1519dfb591c0301de29d472dd0b54cc16ef22c52
hyntk <hoge@hoge.com>
2019-05-28 16:59:44 +0900

    コントローラ及びヘルパーファイルの整形

--- app/controllers/miniblogs_controller.rb
+++ app/controllers/miniblogs_controller.rb
@@ -1,55 +1,55 @@
 class MiniblogsController < ApplicationController
-    before_action :set_miniblog,only: [:edit, :update,:destroy]
+  before_action :set_miniblog,only: [:edit, :update,:destroy]
 
-    def index
-        @miniblogs=Miniblog.all
+  def index
+    @miniblogs=Miniblog.all
+  end
+
+  def new
+    if params[:back]
+      @miniblog=Miniblog.new(miniblog_params)
+    else
+      @miniblog=Miniblog.new
     end
+  end
 
-    def new
-        if params[:back]
-            @miniblog=Miniblog.new(miniblog_params)
-        else
-            @miniblog=Miniblog.new
-        end
-    end
-
-    def create
-        @miniblog=Miniblog.new(miniblog_params)
-        if @miniblog.save
+  def create
+    @miniblog=Miniblog.new(miniblog_params)
+      if @miniblog.save
         redirect_to miniblogs_path,notice:"ミニブログを作成しました！"
-        else
-            render'new'
-        end
+      else
+        render'new'
+      end
     end
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
-        if @miniblog.update(miniblog_params)
-        redirect_to miniblogs_path, notice: "ミニブログを編集しました！"
-        else
-        render 'edit'
-        end
+  def update
+  if @miniblog.update(miniblog_params)
+    redirect_to miniblogs_path, notice: "ミニブログを編集しました！"
+    else
+    render 'edit'
     end
+  end
 
-    def destroy
-        @miniblog.destroy
-        redirect_to miniblogs_path, notice:"ミニブログを削除しました！"
-    end
+  def destroy
+    @miniblog.destroy
+    redirect_to miniblogs_path, notice:"ミニブログを削除しました！"
+  end
 
-    def confirm
-        @miniblog=Miniblog.new(miniblog_params)
-        render :new if @miniblog.invalid?
-    end
+  def confirm
+    @miniblog=Miniblog.new(miniblog_params)
+    render :new if @miniblog.invalid?
+  end
 
-    private
+  private
 
-    def miniblog_params
-        params.require(:miniblog).permit(:content)
-    end
+  def miniblog_params
+    params.require(:miniblog).permit(:content)
+  end
 
-    def set_miniblog
-        @miniblog=Miniblog.find(params[:id])
-    end
+  def set_miniblog
+    @miniblog=Miniblog.find(params[:id])
+  end
 end
--- app/helpers/miniblogs_helper.rb
+++ app/helpers/miniblogs_helper.rb
@@ -1,9 +1,9 @@
 module MiniblogsHelper
-        def choose_new_or_edit
-          if action_name == 'new' || action_name == 'confirm'
-            confirm_miniblogs_path
-          elsif action_name == 'edit'
-            miniblog_path
-          end
-        end
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'
+      confirm_miniblogs_path
+    elsif action_name == 'edit'
+      miniblog_path
+    end
+  end
 end
